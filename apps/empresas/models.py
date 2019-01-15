746ab6894fdfe9513a797abca8191072d8636679
jmdsantos <jmds.marcio@gmail.com>
2019-01-15 11:48:25 -0200

    CRUD empresa parte III editando empresa

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.shortcuts import redirect
+from django.urls import reverse
 
 
 class Empresa(models.Model):
@@ -6,3 +8,8 @@ class Empresa(models.Model):
 
     def __str__(self):
         return self.nome
+
+    def get_absolute_url(selfs):
+        return reverse('home')
+
+
