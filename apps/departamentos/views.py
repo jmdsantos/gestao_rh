a960180514c0a904b74f2dea180988af4b0cab6f
jmdsantos <jmds.marcio@gmail.com>
2019-01-15 18:06:37 -0200

    CRUD departamentos - Delete

--- apps/departamentos/views.py
+++ apps/departamentos/views.py
@@ -1,8 +1,9 @@
+from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
     CreateView,
-    UpdateView
-)
+    UpdateView,
+    DeleteView)
 from .models import Departamento
 
 class DepartamentosList(ListView):
@@ -25,3 +26,7 @@ class DepartamentoCreate(CreateView):
 class DepartamentoUpdate(UpdateView):
     model = Departamento
     fields = ['nome']
+
+class DepartamentoDelete(DeleteView):
+    model = Departamento
+    success_url = reverse_lazy('list_departamentos')
