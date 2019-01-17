20f96f42736a3ba6e82492fe13fb54b9c4960e1b
jmdsantos <jmds.marcio@gmail.com>
2019-01-17 18:51:31 -0200

    Ajustes MEDIA_ROOT

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -109,7 +109,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
